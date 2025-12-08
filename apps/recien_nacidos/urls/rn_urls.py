18fd7ec01c7e2ab21d8c420071b946610c0f94cc
Daniel Valdebenito <danielvaldebenito25@gmail.com>
2025-12-08 15:59:28 -0300

    Terminar de programar el autocompletado del formulario recien nacidos para buscar partos

--- apps/recien_nacidos/urls/rn_urls.py
+++ apps/recien_nacidos/urls/rn_urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from ..views import CrearRecienNacidoView
+from ..views import CrearRecienNacidoView, AutoCompletadoDePartosView
 
 
 app_name = "recien_nacido"
 
 urlpatterns = [
-    path('agregar/', CrearRecienNacidoView.as_view(), name="agregar_recien_nacido")
+    path('agregar/', CrearRecienNacidoView.as_view(), name="agregar_recien_nacido"),
+    path('autocompletado/parto/', AutoCompletadoDePartosView.as_view(), name="autocompletar_parto")
 ]
--- apps/recien_nacidos/views/rn_views.py
+++ apps/recien_nacidos/views/rn_views.py
@@ -1,8 +1,12 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from dal import autocomplete
+from django.db.models.functions import Concat
+from django.db.models import Value, Q, F
 
 from ..forms import RecienNacidoForm
 from ..models import RecienNacido
+from apps.partos.models import Parto
 from core.mixins import MatronaRequiredMixin, MatronaSupervisorRequiredMixin
 
 
@@ -17,4 +21,18 @@ class CrearRecienNacidoView(MatronaRequiredMixin, PermissionRequiredMixin, Creat
         form.instance.created_by = self.request.user
         form.instance.updated_by = self.request.user
         return super().form_valid(form)
-    
+
+
+
+
+class AutoCompletadoDePartosView(MatronaRequiredMixin, autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        partos = Parto.objects.all()
+
+        if self.q:
+            partos = partos.annotate(nombre_completo_paciente=Concat('gestacion__paciente__nombre', Value(' '), 'gestacion__paciente__primer_apellido', Value(' '), 'gestacion__paciente__segundo_apellido'),
+                                     identificacion_paciente=F('gestacion__paciente__identificacion'))
+            
+            return partos.filter(Q(nombre_completo_paciente__icontains=self.q) | Q(identificacion_paciente__startswith=self.q) | Q(pk__startswith=self.q))
+        
+        return partos.none()
