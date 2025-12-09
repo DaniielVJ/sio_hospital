9e65daf63358c17cb13831dce8ed051761d7b2c5
pudusito <benjaminmaldonadobarrales@gmail.com>
2025-12-09 05:11:09 -0300

    cambiada toda la estructura xd

--- apps/dashboard/views/vistas_views.py
+++ apps/dashboard/views/vistas_views.py
@@ -5,6 +5,25 @@ from django.views.generic import TemplateView
 def index(request):
     return render(request, 'index.html')
 
+class PacientesView(TemplateView):
+    template_name = 'layouts/pacientes.html'
+
+class PartosView(TemplateView):
+    template_name = 'layouts/partos.html'
+
+class PerfilesView(TemplateView):
+    template_name = 'layouts/perfiles.html'
+
+class RecienNacidosView(TemplateView):
+    template_name = 'layouts/recien_nacidos.html'
+
+class ReportesView(TemplateView):
+    template_name = 'layouts/reportes.html'
+
+class ProfesionalesView(TemplateView):
+    template_name = 'layouts/profesionales.html'
+
+
 class TestView(TemplateView):
     template_name = '500.html'
 
