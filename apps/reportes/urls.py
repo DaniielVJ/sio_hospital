885b6fad76b8715b182ce4f66744ddfc4a7b1ed5
AlejandroLiinares <alejandrolinaresnt@gmail.com>
2025-12-11 18:29:46 -0300

    Me quedé en enrutar inicio_reportes, login listo, reemplazo del wizard, mejora de diseño de los select en los forms

--- apps/reportes/urls.py
+++ apps/reportes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
+from apps.reportes.views.inicio_views import inicio_reportes
 from .views import GenerarReporteCesarea
 
 app_name = "reportes"
 
 urlpatterns = [
-    path('cesarea/', GenerarReporteCesarea.as_view(), name="crear_reporte_cesarea")
+    path('cesarea/', GenerarReporteCesarea.as_view(), name="crear_reporte_cesarea"),
+    path('', inicio_reportes, name='inicio_reportes'),
 ]
