1080e5e9de3b796e6be7dbc126eb0f85f7c68be5
Daniel Valdebenito <danielvaldebenito25@gmail.com>
2025-12-15 18:22:55 -0300

    Añadir datos al grafico 1 de pacientes dashboard

--- apps/dashboard/static/dashboard/js/pacientes.js
+++ apps/dashboard/static/dashboard/js/pacientes.js
@@ -37,7 +37,7 @@ window.renderChartPaciente1 = function () {
     if (!div) return;
 
     val1= `["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"]`
-    val2= `[180,200,300,260,240,230,150,180,200,220,250,360]`
+    val2= div.dataset.value.split(",")
 
     const meses = JSON.parse(div.dataset.meses || val1);
     const valores = JSON.parse(div.dataset.values || val2);
@@ -82,10 +82,11 @@ window.renderChartPaciente2 = function () {
     if (!div) return;
 
     val1= `["18-25","26-35","36-45","56+"]`
-    val2= `[20,30,10,5]`
+    val2= div.dataset.value.split(',')
+    console.log(val2)
 
     const meses = JSON.parse(div.dataset.meses || val1);
-    const valores = JSON.parse(div.dataset.values || val2);
+    const valores = JSON.parse(div.dataset.value || val2);
 
     const chart = echarts.init(div);
 
@@ -119,6 +120,7 @@ window.renderChartPaciente2 = function () {
     chart.resize();
 };
 
+// PONE COMENTARIOS DE QUE HACE CADA COSA PARA NO TENER QUE IR ARCHIVO EN ARCHIVO
 window.renderChartPaciente3 = function () {
     const div = document.getElementById("chart_paciente_3");
     if (!div) return;
@@ -162,37 +164,37 @@ window.renderChartPaciente3 = function () {
 
 
 
-/* tabla con maximo 10 pacientes */
-window.renderTablePaciente = function () {
-    const tbody = document.getElementById("tabla_paciente");
-    if (!tbody) return;
+// /* tabla con maximo 10 pacientes */
+// window.renderTablePaciente = function () {
+//     const tbody = document.getElementById("tabla_paciente");
+//     if (!tbody) return;
 
-    let tabla = [];
-    try { /* Aqui debemos agregar el valor que definiremos abajo */
-         /* recordar poner la comillas y sin espacios al inyectar datos en bruto*/
-        tabla = JSON.parse(tbody.dataset.value || '[{"nombre":"Ana Perez","edad":28},{"nombre":"Maria Gomez","edad":32}, {"nombre":"Luisa Fernandez","edad":45},{"nombre":"Carmen Rodriguez","edad":36},{"nombre":"Sofia Martinez","edad":29},{"nombre":"Elena Lopez","edad":40},{"nombre":"Marta Sanchez","edad":33}]');
-    } catch (e) {
-        console.error('Error parseando tabla_pacientes dataset:', e);
-        tabla = [];
-    }
+//     let tabla = [];
+//     try { /* Aqui debemos agregar el valor que definiremos abajo */
+//          /* recordar poner la comillas y sin espacios al inyectar datos en bruto*/
+//         tabla = JSON.parse(tbody.dataset.value.split(',') || '[{"nombre":"Ana Perez","edad":28},{"nombre":"Maria Gomez","edad":32}, {"nombre":"Luisa Fernandez","edad":45},{"nombre":"Carmen Rodriguez","edad":36},{"nombre":"Sofia Martinez","edad":29},{"nombre":"Elena Lopez","edad":40},{"nombre":"Marta Sanchez","edad":33}]');
+//     } catch (e) {
+//         console.error('Error parseando tabla_pacientes dataset:', e);
+//         tabla = [];
+//     }
 
-    // limpiar contenido previo
-    tbody.innerHTML = '';
+//     // limpiar contenido previo
+//     tbody.innerHTML = '';
 
 
-    tabla.forEach(paciente => {
-        const row = document.createElement("tr");
+//     tabla.forEach(paciente => {
+//         const row = document.createElement("tr");
 
-        /* nombre tabla = para json */
-        const cellNombre = document.createElement("td");
-        cellNombre.textContent = paciente.nombre || paciente.nombre || ''; /* se puede validar */
-        row.appendChild(cellNombre);
+//         /* nombre tabla = para json */
+//         const cellNombre = document.createElement("td");
+//         cellNombre.textContent = paciente.nombre || paciente.nombre || ''; /* se puede validar */
+//         row.appendChild(cellNombre);
 
-        /* edad */
-        const cellEdad = document.createElement("td");
-        cellEdad.textContent = paciente.edad;
-        row.appendChild(cellEdad);
+//         /* edad */
+//         const cellEdad = document.createElement("td");
+//         cellEdad.textContent = paciente.edad;
+//         row.appendChild(cellEdad);
 
-        tbody.appendChild(row);
-    });
-};
+//         tbody.appendChild(row);
+//     });
+// };
